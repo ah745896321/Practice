60fd7db0f5757b85050c69a741d105b70047594f
ah745896321 <100401028+ah745896321@users.noreply.github.com>
2026-04-28 19:14:29 +0800

    init

---
+++ LeetCode/1. Two Sum/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int index = nums.IndexOf(target - nums[i]);
+            if (index >= 0 && index != i)
+            {
+                return [i, index];
+
+            }
+        }
+        return [0, 0];
+    }
+}
+public class ConsoleApp1
+{
+    public static int Main()
+    {
+        Solution solution = new Solution();
+        Console.Write(solution.TwoSum([2, 7, 11, 15],9));
+        return 0;
+    }
+}
